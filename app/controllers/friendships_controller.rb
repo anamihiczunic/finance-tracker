e50726e5f77359ccb3a064920ea8a885717e44b3
Ana Mihic Zunic <anamihiczunic@gmail.com>
2021-04-03 09:22:07 +0200

    Implement remove tracking and add friend functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,22 +1,20 @@
 class FriendshipsController < ApplicationController
 
   def create
-    # stock = Stock.check_db(params[:ticker])
-    # if stock.blank?
-    #   stock = Stock.new_lookup(params[:ticker])
-    #   stock.save
-    # end
-    # @user_stock = UserStock.create(user: current_user, stock: stock)
-    # flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
-    # redirect_to my_portfolio_path
-
+    friend = User.where(id: params[:friend]).first
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "You are now friends with #{friend.first_name}"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
-    friend = User.find(params[:id])
-    friendship = Friendship.find_by(user_id: current_user.id, friend_id: friend.id )
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.delete
-    flash[:notice] = "#{friend.first_name} was successfully removed from friends list"
+    flash[:notice] = "Stopped following"
     redirect_to my_friends_path
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,38 @@ class User < ApplicationRecord
     "anonymus"
   end
 
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%" )
+  end
+
+  def except_current_user(users)
+    users.reject {|user| user.id == self.id }
+  end
+
+  def not_friends?(friend)
+    !self.friends.present?(friend)
+  end
+
+  def not_friends_with?(user_id)
+    !self.friends.where(id: user_id).exists?
+  end
 end
